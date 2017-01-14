c9ea315f36f2b593b40ab5b4868b32fe61bdd2c7
Jason Mabanta <jasonmabz1234@gmail.com>
2017-01-14 10:07:57 +0800

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -6,16 +6,16 @@ public class Inventory {
     
     public static void main(String[] args) {
         
-        item t1 = new item("Safe Guard", "Soap", 15.00f);
-        item t2 = new item("Coke", "Softdrinks", 12.00f);
-        item t3 = new item("EQ", "Diapers", 8.00f);
-        item t4 = new item("Creamy White", "Coffee", 7.00f);
-        item t5 = new item("Bear Brand", "Milk", 25.00f);
-        item t6 = new item("winston", "Cigarette", 30.00f);
-        item t7 = new item("Hanzel", "Biscuit", 6.00f);
-        item t8 = new item("Whisper", "Napkin", 7.00f);
-        item t9 = new item("Downy", "Fabric Conditioner", 6.00f);
-        item t10 = new item("Piatos", "Junk foods", 10.00f);
+        item t1 = new item("perla", "Soap", 15.00f);
+        item t2 = new item("Virgin Cola", "Softdrinks", 12.00f);
+        item t3 = new item("Adults Diaper, "Diapers", 8.00f);
+        item t4 = new item("Barako", "Coffee", 7.00f);
+        item t5 = new item("Liberty", "Milk", 25.00f);
+        item t6 = new item("Fortune", "Cigarette", 30.00f);
+        item t7 = new item("Honey", "Biscuit", 15.00f);
+        item t8 = new item("Modess", "Napkin", 7.00f);
+        item t9 = new item("Surf", "Fabric Conditioner", 6.00f);
+        item t10 = new item("Boy Bawang", "Junk foods", 15.00f);
           
         List<item> inv = new ArrayList<item>();
         inv.add(t1);
